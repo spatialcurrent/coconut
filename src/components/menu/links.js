7a95f19a087b65cd826b92737520e329e2cdd241
Patrick Dufour <pjdufour.dev@gmail.com>
2018-11-22 00:59:48 -0500

    more links

--- src/components/menu/links.js
+++ src/components/menu/links.js
@@ -2,5 +2,33 @@ export default [
   {
     text: 'Queries',
     to: '/queries',
+  },
+  {
+    text: 'Coconut',
+    to: 'https://github.com/spatialcurrent/coconut',
+  },
+  {
+    text: 'Railgun',
+    to: 'https://github.com/spatialcurrent/railgun',
+  },
+  {
+    text: 'More Open Source',
+    to: 'https://spatialcurrent.io/open-source/',
+  },
+  {
+    text: 'Spatal Current, Inc.',
+    to: 'https://spatialcurrent.io/company/',
+  },
+  {
+    text: 'Privacy Policy',
+    to: 'https://spatialcurrent.io/privacy-policy/',
+  },
+  {
+    text: 'Terms of Service',
+    to: 'https://spatialcurrent.io/terms-of-service/',
+  },
+  {
+    text: 'Security',
+    to: 'https://spatialcurrent.io/security/',
   }
 ];
