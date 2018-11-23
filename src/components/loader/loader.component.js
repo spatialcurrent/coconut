4278035ede17602270c62da97b0f3cec608d4d75
Patrick Dufour <pjdufour.dev@gmail.com>
2018-11-23 14:45:51 -0500

    new loader

--- src/components/loader/loader.component.js
+++ src/components/loader/loader.component.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import styles from './loader.styles.scss';
 
 export default class Loader extends Component {
@@ -12,14 +12,36 @@ export default class Loader extends Component {
     showLoader: PropTypes.bool.isRequired,
   }
 
+  state = {
+    completed: 0,
+  }
+
+  componentDidMount () {
+    this.timer = setInterval(this.progress, 500);
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer);
+  }
+
+  progress () {
+    const { completed } = this.state;
+    if (completed === 100) {
+      this.setState({ completed: 0 });
+    } else {
+      const diff = Math.random() * 10;
+      this.setState({ completed: Math.min(completed + diff, 100) });
+    }
+  }
+
   render () {
     const { closeLoader, showLoader } = this.props;
 
     return (
       <Dialog open={showLoader} onClose={closeLoader}>
-        <DialogTitle>Loading data</DialogTitle>
+        <DialogTitle>Searching ...</DialogTitle>
         <DialogContent className={styles.content}>
-          <CircularProgress size={50} />
+          <LinearProgress variant="determinate" value={this.state.completed} />
         </DialogContent>
       </Dialog>
     );
