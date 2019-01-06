d2a0374a3f26fa80cd188d22ca76c7840b7c5e06
sdpeyton <steve.peyton@arcadiapower.com>
2019-01-06 10:33:28 -0500

    wrap up alert component

--- src/components/alert/alert.component.js
+++ src/components/alert/alert.component.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import styles from './alert.styles.scss';
 
@@ -18,8 +21,15 @@ export default class Loader extends Component {
       <Dialog open={!!alert} onClose={closeAlert}>
         <DialogTitle>Oops</DialogTitle>
         <DialogContent className={styles.content}>
-          { alert }
+          <DialogContentText>
+            { alert }
+          </DialogContentText>
         </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={closeAlert} color="primary">
+            Got it
+          </Button>
+        </DialogActions>
       </Dialog>
     );
   }
