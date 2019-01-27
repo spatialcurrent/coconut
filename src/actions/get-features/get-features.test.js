80b0f60b7929bb743a2504cd217d606ecc4df71a
sdpeyton <steve.peyton@arcadiapower.com>
2019-01-27 15:11:01 -0500

    update get features tests

--- src/actions/get-features/get-features.test.js
+++ src/actions/get-features/get-features.test.js
@@ -1,10 +1,11 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import {
-  OPEN_LOADER,
-  GET_QUERY,
-  GET_FEATURES,
   CLOSE_LOADER,
+  GET_FEATURES,
+  GET_QUERY,
+  OPEN_ALERT,
+  OPEN_LOADER,
 } from 'action-types';
 import {
   mockExecuteServiceResult,
@@ -14,15 +15,18 @@ import {
 import getFeatures from './get-features.action';
 
 const mockStore = configureMockStore([thunk]);
+let store;
 
 describe('Action: getFeatures', () => {
-  it('creates open loader, get features, get query, and close loader actions', async () => {
-    const store = mockStore({
+  beforeEach(() => {
+    store = mockStore({
       features: null,
       query: null,
       showLoader: null,
     });
+  });
 
+  it('creates open loader, get features, get query, and close loader actions', async () => {
     const params = { service: 'test-service' };
     const expectedActions = [
       { type: OPEN_LOADER },
@@ -33,5 +37,27 @@ describe('Action: getFeatures', () => {
 
     const updatedStore = await store.dispatch(getFeatures(params));
     expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('creates open alert action when features return empty array', async () => {
+    const params = { service: 'test-service', noFeatures: true };
+    const expectedActions = [
+      { type: OPEN_LOADER },
+      { type: OPEN_ALERT, message: 'There were no results found for this query. Please try another query.' },
+      { type: CLOSE_LOADER },
+    ];
+
+    const updatedStore = await store.dispatch(getFeatures(params));
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('creates open alert action when recieving an error', async () => {
+    const params = { servie: 'test-service', withError: true };
+
+    const expectedActions = [
+      { type: OPEN_LOADER },
+      { type: OPEN_ALERT, message: 'There was a problem executing this query. We have been notified of the problem and will work to fix it.' },
+      { type: CLOSE_LOADER },
+    ];
   });
 });
