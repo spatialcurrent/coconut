2cc396eace2f4c67fd2d9c4b9234d57b628ce37b
sdpeyton <steve.peyton@arcadiapower.com>
2019-01-30 22:32:55 -0500

    fix notes reducer tests

--- src/actions/get-query/get-query.test.js
+++ src/actions/get-query/get-query.test.js
@@ -4,7 +4,7 @@ import {
   mockGetProcessResult,
   mockGetServiceResult,
 } from 'api-client';
-import { GET_QUERY } from 'action-types';
+import { ADD_NOTE, GET_QUERY } from 'action-types';
 import getQuery from './get-query.action';
 
 const mockStore = configureMockStore([thunk]);
@@ -13,10 +13,16 @@ describe('Action: getQuery', () => {
   it('creates a get query action', async () => {
     const store = mockStore({ query: null });
     const updatedStore = await store.dispatch(getQuery('test-service'));
-    const expectedAction = {
-      query: { ...mockGetProcessResult, ...mockGetServiceResult },
-      type: GET_QUERY
-    };
-    expect(store.getActions()).toEqual([expectedAction]);
+    const expectedActions = [
+      {
+        query: { ...mockGetProcessResult, ...mockGetServiceResult },
+        type: GET_QUERY,
+      },
+      {
+        note: {id: 'load-layer', message: 'Zoom in to view layer'},
+        type: ADD_NOTE,
+      }
+    ];
+    expect(store.getActions()).toEqual(expectedActions);
   });
 });
--- src/reducers/notes/notes.test.js
+++ src/reducers/notes/notes.test.js
@@ -5,14 +5,16 @@ describe('Reducer: notes', () => {
   it('adds note', () => {
     const note = { id: 1 };
     const action = { note, type: ADD_NOTE };
+    const expectedState = [{ id: 1, isOpen: true }];
     const state = notesReducer(undefined, action);
-    expect(state).toEqual([note]);
+    expect(state).toEqual(expectedState);
   });
 
   it('removes note', () => {
-    const initialState = [{ id: 1 }, { id: 2 }];
+    const initialState = [{ id: 1, isOpen: true}, { id: 2, isOpen: true }];
+    const expectedState = [{ id: 2, isOpen: true }, { id: 1, isOpen: false }];
     const action = { id: 1, type: REMOVE_NOTE };
     const state = notesReducer(initialState, action);
-    expect(state).toEqual([{ id: 2 }]);
+    expect(state).toEqual(expectedState);
   });
 });
